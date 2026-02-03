5d08c06c179072d3cfe3ffe21ef7b03400782625
João neto <netinhooo.537@gmail.com>
2026-02-03 12:21:20 -0300

    Update Stripe API key in stripeConfig.js

--- src/config/stripeConfig.js
+++ src/config/stripeConfig.js
@@ -1,5 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromisse = loadStripe('sk_test_51SBe4lK7tAU1ctfdABHIWMhCppWPWoJ0YQPbwlS7YsYRi3i1Z0dtou1PxJsOC3LKDGZmWRYN321tzZp6vEi6lSNB00yyJ4m6WYclea')
+const stripePromisse = loadStripe('sk_test_51SBe4lK7tAU1ctfdeNgZ7mHKnpJi4JMNZpPZyOI86219TwlOQyKAlF5SkhG5GtvTrHqPmyXd1h3ssD01K549z0C100fMqwLcwp')
 
 export default stripePromisse
