86269934657ac9adbb86c7fa0386c490e9113d2f
netinho537ia <netinhooo.537@gmail.com>
2026-02-13 16:33:54 -0300

    stripe atualização

--- src/config/stripeConfig.js
+++ src/config/stripeConfig.js
@@ -1,5 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromisse = loadStripe('pk_test_51SBe4lK7tAU1ctfdyk73diOvXoWL2UiJgS6o6w1qDDUHTPfmoBxSEaGHYgILwO1gwBU1yeOMWXSS1mOFZzVQ9ShO00pHqPfhVx')
+const stripePromisse = loadStripe(process.env.STRIPE_SECRET_KEY)
 
 export default stripePromisse
